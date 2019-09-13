85c90ca29633ea5a906cc83629351bf6c59dcb2f
Kaiisha05 <Kaiisha@yahoo.com>
2019-09-13 16:10:38 -0400

    removed the grid div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,5 @@
 document.getElementById("number-to-guess").innerHTML = [Math.floor(Math.random() * 121)+19];
 // console.log("this is working");
+// need a function that will pick and display the random number on clicking each shape
+[Math.floor(Math.random() * 13)];
+console.log("this works too");
