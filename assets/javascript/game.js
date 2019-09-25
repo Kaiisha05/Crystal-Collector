2ee8a6827f0cab4c63c969a12f96f8b3cb3e0a42
Kaiisha05 <Kaiisha@yahoo.com>
2019-09-25 13:26:09 -0400

    spellcheck and moved variables to the top of the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
-// document.getElementById("number-to-guess").innerHTML = [Math.floor(Math.random() * 121) + 19];
-
+var counter;
+var wins = 0;
+var losses = 0;
+var targetNumber;
+var numbers = [];
+//  make the images part of an array that we can reference later
+var shapes = ["assets/images/blue hexagon.jpg", "assets/images/clear square.jpeg", "assets/images/green heart.jpg", "assets/images/pink diamond.jpeg"]
+console.log(shapes);
+// still need to images to be displayed on the screen
 // need a function that will pick and display the random number on clicking each shape
-
 
 // $(".crystal").on("click", function () {
 //     (Math.floor(Math.random() * 13));
@@ -9,37 +15,77 @@
 
 // my work above
 
-var targetNumber = Math.floor(Math.random() * 121) + 19;
 
-  $("#number-to-guess").text(targetNumber);
+targetNumber = Math.floor(Math.random() * 121) + 19;
+console.log(targetNumber)
+$("#number-to-guess").text(targetNumber);
 
-// //   var counter = 0;
+// document.getElementById("number-to-guess").innerHTML = [Math.floor(Math.random() * 121) + 19];
+
+
+
+
+
+
 
 // // Now for the hard part. Creating multiple crystals each with their own unique number value.
+// var blue = 1
+// var clear = 2
+// var green = 3
+// var pink = 4
 
-// // We begin by expanding our array to include four options.
-var numberOptions = Math.floor(Math.random() * 13)
-    console.log(numberOptions);
-var imageCrystal = $("<img>");
-imageCrystal.addClass("crystal-image");
-imageCrystal.attr("src", "assets/images/blue hexagon.jpg");
-imageCrystal.attr("data-crystalvalue", numberOptions);
-$("#crystals").append(imageCrystal);
 
-var imageCrystal1 = $("<img>");
-imageCrystal1.addClass("crystal-image");
-imageCrystal1.attr("src", "assets/images/clear square.jpeg");
-imageCrystal1.attr("data-crystalvalue", numberOptions);
-$("#crystals").append(imageCrystal1);
 
-var imageCrystal2 = $("<img>");
-imageCrystal2.addClass("crystal-image");
-imageCrystal2.attr("src", "assets/images/green heart.jpg");
-imageCrystal2.attr("data-crystalvalue", numberOptions);
-$("#crystals").append(imageCrystal2);
+// for (var i = 0; i < 4; i++) {
+    // why do i have this variable twice?
+    //    var imageCrystal = $("<div>");
+    // imageCrystal.addClass("col-sm-3 A");
 
-var imageCrystal3 = $("<img>");
-imageCrystal3.addClass("crystal-image");
-imageCrystal3.attr("src", "assets/images/pink diamond.jpeg");
-imageCrystal3.attr("data-crystalvalue", numberOptions);
-$("#crystals").append(imageCrystal3);
+//     var imageCrystal = $("<img>");
+//     imageCrystal.addClass("crystal-image");
+
+
+//     imageCrystal.attr("src", shapes[i]);
+
+//     imageCrystal.attr("data-blue", numbers[i]);
+
+//     $(".col").append(imageCrystal);
+
+
+// }
+// iterate through 4 numbers that are randomly selected between 1-12. Then assign those random numbers to the var numbers that will hold them as an array. Established the var numbers as an array by setting it equal to empty brackets
+for (var s = 0; s < 4; s++) {
+    var shapeNumbers = (Math.floor(Math.random() * 13) + 1);
+    console.log(shapeNumbers);
+
+    numbers.push(shapeNumbers);
+    console.log(numbers);
+}
+
+// var imageCrystal1 = $("<img>");
+// imageCrystal1.addClass("crystal-image");
+// imageCrystal1.attr("src", "assets/images/clear square.jpeg");
+// imageCrystal1.attr("data-crystalvalue", shapes);
+// $("#crystals").append(imageCrystal1);
+
+// var imageCrystal2 = $("<img>");
+// imageCrystal2.addClass("crystal-image");
+// imageCrystal2.attr("src", "assets/images/green heart.jpg");
+// imageCrystal2.attr("data-crystalvalue", shapes);
+// $("#crystals").append(imageCrystal2);
+
+// var imageCrystal3 = $("<img>");
+// imageCrystal3.addClass("crystal-image");
+// imageCrystal3.attr("src", "assets/images/pink diamond.jpeg");
+// imageCrystal3.attr("data-crystalvalue", shapes);
+// $("#crystals").append(imageCrystal3);
+
+$("data-blue").on("click", function () {
+    var crystalValue = $((this).attr("data-blue"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+
+    document.getElementById("#game-tally").innerHTML = counter;
+});
+
+
